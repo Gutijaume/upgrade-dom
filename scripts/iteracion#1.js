a6f58125cec65e02e77bf91b0f5acad9726caced
gutijaume <gutijaume@hotmail.com>
2021-12-19 13:41:49 +0100

    Create iteracion#1.js
    Mostrar en consola los siguientes elementos del html

---
+++ scripts/iteracion#1.js
@@ -0,0 +1,37 @@
+window.onload = () =>{
+//1.1 Usa querySelector para mostrar por consola el botón con la clase .showme
+let bttn$$ = document.querySelector(".showme")
+console.log(bttn$$)
+
+// 1.2 Usa querySelector para mostrar por consola el h1 con el id #pillado
+
+let title1$$ = document.querySelector("#pillado")
+console.log(title1$$)
+
+//1.3 Usa querySelector para mostrar por consola todos los p
+
+let allP$$ = document.querySelectorAll("p")
+console.log(allP$$)
+
+// 1.4 Usa querySelector para mostrar por consola todos los elementos con la clase.pokemon
+
+let AllPokemonClass$$ = document.querySelectorAll(".pokemon")
+console.log(AllPokemonClass$$)
+
+//1.5 Usa querySelector para mostrar por consola todos los elementos con el atributo 
+//data-function="testMe".
+
+let testMeFunctions$$ = document.querySelectorAll('[data-function ="testMe"]')
+console.log(testMeFunctions$$)
+
+//1.6 1.6 Usa querySelector para mostrar por consola el 3 personaje con el atributo 
+//data-function="testMe".
+
+let thirdItem$$ = document.querySelectorAll('[data-function ="testMe"]')
+        for (let i = 0; thirdItem$$.length; i++){
+            if (i===3){
+                console.log(thirdItem$$[i])
+                break
+            }
+        }
+}
