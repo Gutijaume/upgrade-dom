93ce86eb303208acca6042b1bdc323a9a15cbf86
gutijaume <gutijaume@hotmail.com>
2021-12-21 07:42:15 +0100

    Update iteracion2.js
    Correciones echas y añadidos los últimos ejercicios repasados en clase

--- scripts/iteracion2.js
+++ scripts/iteracion2.js
@@ -18,12 +18,12 @@ for (let i=0; i < 6; i++){
 }
 //2.4 Inserta dinamicamente con javascript en un html una p con el texto 'Soy dinámico!'.
 let ptext$$ = document.createElement("p")
-ptext$$.innerHTML = "Soy dinamico!"
+ptext$$.innerText = "Soy dinamico!"
 document.body.append(ptext$$)
 
 //2.5 Inserta en el h2 con la clase .fn-insert-here el texto 'Wubba Lubba dub dub'.
-let h2$$ = document.querySelector("h2")
-h2$$.innerHTML = 'Wubba Lubba dub dub'
+let h2$$ = document.querySelector("h2.fn-insert-here")
+h2$$.innerText = 'Wubba Lubba dub dub'
 
 //2.6 Basandote en el siguiente array crea una lista ul > li con los textos del array.
 
@@ -38,10 +38,42 @@ for (let app of apps){
     list$$.appendChild(appLi$$)
 }
 
+// apps.forEach((item)=>{
+//     let li$$ = document.createElement("li")
+//     li$$.innerText = item
+//     li$$.appendChild(li$$)
+// }) Es lo mismo pero con un foreach
+
+
 //2.7 Elimina todos los nodos que tengan la clase .fn-remove-me
 
-let removeClass = document.querySelectorAll (".fn-remove-me")
-removeClass.remove(".fn-remove-me")
+let removeClass$$ = document.querySelectorAll.remove (".fn-remove-me")
+
+removeClass$$.forEach((value) =>{
+    value.delete()
+})
+
+// for (let value of removeClass$$){
+//     value.delete()
+// } Y aquí lo mismo con un forOf
+
+// 2.8 Inserta una p con el texto 'Voy en medio!' entre los dos div. 
+// 	Recuerda que no solo puedes insertar elementos con .appendChild.
+
+let allMyDivs$$ = document.querySelectorAll("div")
+let newP$$ = document.createElement("p")
+newP$$.innerText = "Voy en medio!"
+body.insertBefore(newP$$, allMyDivs$$[1])
+
+//2.9 Inserta p con el texto 'Voy dentro!', dentro de todos los div con la clase .fn-insert-here
+
+let divsWithText$$ = querySelectorAll("div.fn-insert-here")
+
+for (let div of divsWithText$$){
+    div.innerHTML = "<p>Voy dentro!</p>"
+}
+
+
 
 
 
